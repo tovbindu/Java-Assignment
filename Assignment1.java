3fe530e6aa23bdf9d3ee654529e35774b8f08256
tovbindu <47753632+tovbindu@users.noreply.github.com>
2019-02-22 22:49:38 -0500

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,13 @@
+package corejavapack1;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a = 10, b = 20;
+     System.out.println("The sum of a and b is " + (a+b));
+      
+	}
+
+}
+  
