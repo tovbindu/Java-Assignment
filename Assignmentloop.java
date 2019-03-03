6861c913605140c8caec87934d75baa3b377cb2c
tovbindu <47753632+tovbindu@users.noreply.github.com>
2019-03-02 23:00:12 -0500

    Add files via upload

--- Assignmentloop.java
+++ Assignmentloop.java
@@ -2,19 +2,39 @@ package assignmentpack;
 
 import java.util.Scanner;
 
-public class Assignmentloop {	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//public class Assignmentloop {	
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Scanner input = new Scanner(System.in);
+//		System.out.print("Enter any number: ");
+//		int a = input.nextInt();
+//		System.out.println("The number entered is " +a);
+//		int fact = 1;
+//		for(int i= 1; i<=a; i++){
+//			fact = fact *i;
+//		}
+//		System.out.println("the factorial of the number "+ a + " is " +fact);
+//
+//	}
+//
+//}
+
+public class Assignmentloop{
+	public static void main(String []args){
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter any number: ");
-		int a = input.nextInt();
-		System.out.println("The number entered is " +a);
-		int fact = 1;
-		for(int i= 1; i<=a; i++){
-			fact = fact *i;
+		System.out.print("Enter the first number: ");
+		int i = input.nextInt();
+		System.out.print("Enter the second number: ");
+		int j = input.nextInt();
+	 System.out.println("The first number is "+i);
+	 System.out.println("The second number is "+j);
+	 for (i=1; i>=0;i++){
+		 for (j = 1; j>=0; j++){
+			 
+		 }
+		 
+			
+			
 		}
-		System.out.println("the factorial of the number "+ a + " is " +fact);
-
 	}
-
 }
